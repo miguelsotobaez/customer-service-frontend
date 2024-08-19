0590d3b2ed22c954811cc3236c9694b91f619867
Miguel Soto Baez <miguelsotobaez@gmail.com>
2024-08-19 16:26:45 +0100

    update testing

--- src/app/services/chat.service.spec.ts
+++ src/app/services/chat.service.spec.ts
@@ -13,8 +13,46 @@ describe('ChatService', () => {
 
   const mockRepresentative = { id: 1, name: 'Alice', isAvailable: true };
   const mockTopics = [
-    { name: 'General Inquiry', suggestions: [{ name: 'Sub-topic 1' }] },
-    { name: 'Technical Support', suggestions: [{ name: 'Sub-topic 2' }] },
+    {
+      name: 'Football',
+      suggestions: [
+        {
+          name: 'Premier League',
+          suggestions: [
+            { name: 'Liverpool' },
+            { name: 'Man. UTD' },
+            { name: 'Man. City' },
+          ],
+        },
+        {
+          name: 'Serie A',
+          suggestions: [
+            { name: 'Milan' },
+            { name: 'Inter' },
+            { name: 'Juventus' },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Books',
+      suggestions: [
+        {
+          name: 'Investment',
+          suggestions: [
+            { name: 'The Intelligent Investor - Benjamin Graham' },
+            { name: 'Rich Dad, Poor Dad - Robert Kiyosaki' },
+          ],
+        },
+        {
+          name: 'Children',
+          suggestions: [
+            { name: 'Momo - Michael Ende' },
+            { name: 'BFG - Roald Dahl' },
+          ],
+        },
+      ],
+    },
   ];
 
   beforeEach(() => {
